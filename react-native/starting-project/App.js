e69179312875d854f5bfb72796d44f254b5709a8
Junwon-Park <upjw0429@gmail.com>
2022-08-24 03:08:05 +0900

    컨텐츠 목록에 ScrollView 적용

--- react-native/starting-project/App.js
+++ react-native/starting-project/App.js
@@ -1,15 +1,61 @@
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react"; // useState는 react 라이브러리의 useState 객체를 그 대로 사용한다.
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    // JSX의 TextInput의 onChangeText가 입력한 값을 인자로 넘겨준다.
+    // 파라미터의 이름은 당연히 자유롭게 지을 수 있다.
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+    // 상태 갱신 함수를 사용해서 State를 갱신할 때, 갱신할 값이 기존의 State에 의존한다면,
+    // 즉 기존의 State는 그 대로 유지하면서 갱신하는 경우라면 위 처럼 익명 함수로 기존 함수를 전달하는 방식으로 진행하는 것이 올바른 방법이다.
+    // 상태 갱신 함수의 인자로 익명함수를 지정하면 익명함수의 인자로 해당 갱신함수의 현재 State를 받을 수 있는데, 그 것을 이용하는 것이고 이 방법이 추천되는 방법이다.
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" />
-        {/* Button 컴포넌트에는 style 속성이 없기 때문에 직접 스타일을 지정할 수 없다. */}
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        {/* 해당 인풋 컴포넌트에서 발생하는 입력 이벤트는 입력된 값을 지정한 함수의 인자로 전달한다. */}
+        <Button title="Add Goal" onPress={addGoalHandler} />
+        {/* RN에서 Button 컴포넌트의 클릭 이벤트는 onPress라는 속성에 지정한다. */}
       </View>
       <View style={styles.goalContainer}>
-        <Text>List of goal...</Text>
+        {/* ScrollView를 사용할 때에는 <View> 컨테이너 컴포넌트로 감싸서 스크롤이 적용될 부분의 공간을 만들어 줘야 한다. */}
+        {/* ScrollView는 부모 컴포넌트에 맞게 적용된다. */}
+        <ScrollView>
+          {courseGoals.map((goal, idx) => {
+            return (
+              <View style={styles.goalItem} key={idx}>
+                {/* iOS에서는 Text 요소에 직접 둥근 모서리 속성을 적용할 수 없기 때문에 <View> 컴포넌트로 감싸서 해당 컴포넌트에 해당 속성을 적용했다. */}
+                {/* <View> 컴포넌트에 해당 속성을 적용하면 양쪽 Native 플랫폼에 모두 잘 적용된다. */}
+                <Text style={styles.goalItem}>{goal}</Text>
+                {/* RN에서는 CSS 처럼 부모의 스타일 속성이 자식에게 상속되지 않기 때문에 <View> 컴포넌트에 지정한 속성이 <Text> 컴포넌트에 적용되지 않아 텍스트 색상인 color 속성이 적용되지 않았다. */}
+                {/* 그래서 동일한 스타일을 적용해주었다.. */}
+              </View>
+            );
+          })}
+        </ScrollView>
       </View>
     </View>
   );
@@ -51,5 +97,12 @@ const styles = StyleSheet.create({
     flex: 4,
     // flex 속성은 같은 부모 아래 존재하는 형제 컴포넌트 끼리의 비율을 지정한다.
     // 이 컴포넌트는 총 합 4 중에 3의 비율(4/5)을 차지하게 된다.
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
   },
 });
