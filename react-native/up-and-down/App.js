02735552cc03e70f2c65cd3ef462d5665017b1b0
Junwon-Park <upjw0429@gmail.com>
2022-08-25 17:02:17 +0900

    LinearGradient 패키지 설치 및 적용

--- react-native/up-and-down/App.js
+++ react-native/up-and-down/App.js
@@ -1,19 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 // Components
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      {/* 반드시 color 속성으로 사용될 색상을 지정해주어야 하고 값은 배열 형태로 주어야 한다. */}
+      {/* 그리고 배열의 색상에대한 값은 반드시 두 개 이상이어야 한다. */}
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
   },
 });
