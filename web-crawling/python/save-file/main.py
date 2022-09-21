5c47852185347b04ced3c1b9a3010e6be9132a57
Junwon-Park <upjw0429@gmail.com>
2022-09-22 00:15:58 +0900

    크롤링 한 데이터 파일에 저장 구현

--- web-crawling/python/save-file/main.py
+++ web-crawling/python/save-file/main.py
@@ -32,15 +32,24 @@ driver.find_element(By.CLASS_NAME, 'btn_search').click() # 속성의 class가 bt
 time.sleep(1) # 위 작업이 끝날 때 까지 기다려야하기 때문에 1초 동안 기다려준 것이다.
 # 위 작업이 진행되지 않고 아래 작업으로 넘어가 정상적으로 작업이 수행되지 않는 경우 time.sleep()으로 잠시 기다려준다.
 
+orig_stdout = sys.stdout
+f = open(f_name, 'a', encoding='UTF-8') # 크롤링에는 반드시 계속 추가하는 'a'모드를 사용한다.
+sys.stdout = f
+
 html = driver.page_source # 크롬 드라이버로 현재 페이지의 HTML 소스 파일을 가져온다.
 soup = BeautifulSoup(html, 'html.parser') # 위에서 가져온 HTML 소스 파일을 BeautifulSoup에 넣고 html-parser로 분석한다.
 blog_list = soup.find('ul','list_thumType type1') # BeautifulSoup으로 분석한 HTML 소스 파일의 ul 태그를 찾아서 ul 태그와 그 자식 요소들을 불러온다.
 
-# print(blog_list)
+time.sleep(1)
 
 for i in blog_list:
     print(i.text.strip())
     print('\n')
+
+sys.stdout = orig_stdout
+f.close()
+
+print('요청하신 데이터 수집 작업이 정상적으로 완료되었습니다.')
 
 while(True): pass # 크롬 브라우저 실행 유지
 # selenium에서 driver.get() 함수의 실행이 종료되면 브라우저도 종료된다.
