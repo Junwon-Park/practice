441b0417e92129ca3a19a5249019880513899d4c
Junwon-Park <upjw0429@gmail.com>
2022-10-21 16:32:45 +0900

    커스텀 데코레이터 생성 및 사용 정리

--- nest/study-nest-basic-architecture/src/auth/get-user.decorator.ts
+++ nest/study-nest-basic-architecture/src/auth/get-user.decorator.ts
@@ -1,11 +1,13 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from './user.entity';
 
+// ? 요청 객체의 user 객체만 추출해서 반환하는 커스텀 데코레이터
+
 export const GetUser = createParamDecorator(
   // createParamDecorator()는 @Param 데코레이터를 만들 수 있는 메서드이다.
   (data, ctx: ExecutionContext): User => {
-    const req = ctx.switchToHttp().getRequest();
+    const req = ctx.switchToHttp().getRequest(); // 요청 객체를 가져오는 메서드
 
-    return req.user;
+    return req.user; // 요청의 user 객체만 추출해서 반환
   },
 );
