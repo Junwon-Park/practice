9786db99ee256b91391089bad3d459a0ee7ab047
Junwon-Park <upjw0429@gmail.com>
2023-03-02 11:33:59 +0900

    Fix. Graphql config 모듈화

--- side-projects/aniwhere/server/src/interface/graphql/graphql.config.module.ts
+++ side-projects/aniwhere/server/src/interface/graphql/graphql.config.module.ts
@@ -9,21 +9,22 @@ const graphqlErrorLog = new Logger('GraphQLErrorLog');
 export const DefaultGraphQLModule = (path: string): DynamicModule => {
   return GraphQLModule.forRootAsync<ApolloDriverConfig>({
     driver: ApolloDriver,
-    imports: [GraphqlModule],
+    imports: [GraphqlModule], // Resolver를 가지고 있는 모듈 import(imports에 등록한 모듈에서 Resolver들을 exports에 등록하지 않아도 인식한다.(일단 현재 까지는?))
     useFactory: () => ({
       uploads: false,
       cors: true,
       validate: true,
-      playground: true,
-      autoSchemaFile: true,
+      playground: true, //  GraphQL playground 활성화
+      autoSchemaFile: true, // gql 파일에 스키마를 생성하지 않고, 앱 실행 시 메모리에 자동 생성(Default: false)
       debug: true,
       // introspection: false,
       formatError: (error: GraphQLError): GraphQLFormattedError => {
-        graphqlErrorLog.error(error);
-        return error;
+        graphqlErrorLog.error(error); // 서버 찍히는 로그
+        return error; // 클라이언트에 내려주는 에러 객체
       },
       disableHealthCheck: true,
-      path: path,
+      path: path, // URL end point
+      // TODO: 아래 context 옵션은 나중에 미들웨어(가드, 인터셉터 등) 달 때, ExecutionContext(req, res) 잘 가져와 지는 지 확인해보기(GraphQL의 ExecutionContext를 여기에서 가져오는 것인지 확인 필요)
       context: ({ req, res }) => ({ req, res }),
     }),
   });
